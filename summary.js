0f97ff47a9f29342f3dfefbbd8b47a5f4a175b01
Jahangir Alom Minto <jaminto.webdev@gmail.com>
2021-08-20 12:51:14 +0600

    summary.js file done

---
+++ summary.js
@@ -0,0 +1,11 @@
+var i = 0;
+while (i < 7) {
+    console.log('With while loop' + ' ' + i);
+    i++;
+}
+
+
+for (var i = 0; i < 7; i++) {
+    console.log('With for loop' + ' ' + i);
+    i++;
+}
--- while-loop.js
+++ while-loop.js
@@ -1,3 +1,5 @@
+// Note: In while loop, you must change the value of your variable inside the {} second bracket or curly braces
+
 var roastGiven = 0;
 
 while (roastGiven < 7 ) {
